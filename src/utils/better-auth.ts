2081adfcb171824b6a747aa4f6804d950fbe5459
bijan_pradhananga <bijan2059@gmail.com>
2025-12-01 11:14:04 +0545

    imported sendMagicLinkEmail and called the function

--- src/utils/better-auth.ts
+++ src/utils/better-auth.ts
@@ -2,14 +2,16 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { db } from "@/db/mongo.js";
 import { magicLink, openAPI } from "better-auth/plugins";
+import { sendMagicLinkEmail } from "@/utils/services/emailService.js";
 
 export const auth = betterAuth({
   trustedOrigins: ["http://localhost:5173", "http://127.0.0.1:5173"],
   plugins: [
     openAPI({ disableDefaultReference: true }),
     magicLink({
-      sendMagicLink: ({ email, url, token }, ctx) => {
-        console.log(`\n\n✨ Magic Link for ${email}: ${url}\n\n`);
+      sendMagicLink: async ({ email, url, token }, ctx) => {
+        // console.log(`\n\n✨ Magic Link for ${email}: ${url}\n\n`);
+        await sendMagicLinkEmail(email, url);
         ctx?.setCookie("test", token, {
           httpOnly: true,
           secure: true,
